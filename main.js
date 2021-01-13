1a5de07f5e2a92ee7bdbe44dfa38228d597106d0
dongree <dongwoo0307@naver.com>
2021-01-13 20:20:38 +0900

    Improve performance

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ const text = document.querySelector('.text');
 const target = document.querySelector('.target');
 const horizon = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeigth = targetRect.height / 2;
 
 document.addEventListener('mousemove', (e)=>{
     const x = e.clientX;
     const y = e.clientY;
     text.innerHTML = `${x}px, ${y}px`;
-    text.style.top = `${y+10}px`;
-    text.style.left = `${x+10}px`;
-    target.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    horizon.style.top = `${y}px`;
-    vertical.style.left = `${x}px`;
+    text.style.transform = `translate(${x+20}px, ${y+20}px)`;
+    target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeigth}px)`;
+    horizon.style.transform = `translateY(${y}px)`;
+    vertical.style.transform = `translateX(${x}px)`;
 })
